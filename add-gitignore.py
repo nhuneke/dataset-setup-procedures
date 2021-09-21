806d03a54984158a880383f8f25e7bfcbab05278
nh6g15 <n.huneke@soton.ac.uk>
2021-09-21 12:10:49 +0100

    Add first draft gitignore procedure

---
+++ add-gitignore.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+"""
+Procedure to add .gitignore template
+"""
+
+import sys
+import os.path as op
+
+ds = sys.argv[1]
+
+filepath = op.join(ds.path, .gitignore)
+
+ignorecontent = """\
+# History files
+.Rhistory
+.Rapp.history
+
+# Session Data files
+.RData
+
+# User-specific files
+.Ruserdata
+
+# Example code in package build process
+*-Ex.R
+
+# RStudio files
+.Rproj.user/
+
+# knitr and R markdown default cache directories
+*_cache/
+/cache/
+
+# Temporary files created by R markdown
+*.utf8.md
+*.knit.md
+
+# Temporary directory created by dcm2bids
+tmp_dcm2bids/
+"""
+
+file = open(filepath, 'w+')
+file.writelines(ignorecontent)
+file.close
+
+ds.save(message="Apply add .gitignore procedure")
