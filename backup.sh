a14e036f2211b7c4feaa7d475cb86b46974ec5c8
nh6g15 <n.huneke@soton.ac.uk>
2021-09-22 10:19:55 +0100

    Write procedure to set up backup siblings

--- backup.sh
+++ backup.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 # Include RIA backup and gitlab siblings here
+
+set -e -u
+
+echo "Creating RIA sibling..."
+echo
+read -p 'URL for RIA backup: ' riaurl
+
+datalad create-sibling-ria -s ria-backup ria+$riaurl
+
+echo "Creating gitlab sibling..."
+echo
+read -p 'GitLab project location: ' gitlabproj
+
+datalad create-sibling-gitlab -s gitlab --site soton --project $gitlabproj \
+	--publish-depends ria-backup 
