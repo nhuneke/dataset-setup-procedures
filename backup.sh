2242290f097baf0175d150b50fc2d1bd618ad8f6
nh6g15 <n.huneke@soton.ac.uk>
2021-09-22 10:40:13 +0100

    Add instructions to backup procedure

--- backup.sh
+++ backup.sh
@@ -6,12 +6,16 @@ set -e -u
 
 echo "Creating RIA sibling..."
 echo
+echo "Please enter the URL for the RIA backup sibling e.g."
+echo "ssh://ssh.soton.ac.uk:/research/datasets/"
 read -p 'URL for RIA backup: ' riaurl
 
 datalad create-sibling-ria -s ria-backup --existing skip ria+$riaurl
 
 echo "Creating gitlab sibling..."
 echo
+echo "Please enter your GitLab project location. Should take the form of"
+echo "<Project>/<dataset>"
 read -p 'GitLab project location: ' gitlabproj
 
 datalad create-sibling-gitlab -s gitlab --site soton --project $gitlabproj \
