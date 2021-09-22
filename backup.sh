e0c446899d5cc8c43d2af3bc8a67f274d9571ad5
nh6g15 <n.huneke@soton.ac.uk>
2021-09-22 10:22:34 +0100

    Update backup procedure to skip existing siblings

--- backup.sh
+++ backup.sh
@@ -8,11 +8,11 @@ echo "Creating RIA sibling..."
 echo
 read -p 'URL for RIA backup: ' riaurl
 
-datalad create-sibling-ria -s ria-backup ria+$riaurl
+datalad create-sibling-ria -s ria-backup --existing skip ria+$riaurl
 
 echo "Creating gitlab sibling..."
 echo
 read -p 'GitLab project location: ' gitlabproj
 
 datalad create-sibling-gitlab -s gitlab --site soton --project $gitlabproj \
-	--publish-depends ria-backup 
+	--publish-depends ria-backup --existing skip
