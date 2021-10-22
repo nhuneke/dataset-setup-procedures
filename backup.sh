dc0be1d957c75e6a50214389b773b40e984f2e7d
Nathan <n.huneke@soton.ac.uk>
2021-10-22 15:15:53 +0100

    Fix line 26 in backup.sh

--- backup.sh
+++ backup.sh
@@ -22,7 +22,7 @@ read -p 'GitLab project location: ' gitlabproj
 datalad create-sibling-gitlab -s gitlab --site soton --project $gitlabproj \
 	--publish-depends ria-backup --existing skip
 	
-git config -f .datalad/config "datalad.get.subdataset-source-candidate-origin" \ 
-"ria+${riaurl}#{id}"
+git config -f .datalad/config "datalad.get.subdataset-source-candidate-origin" \
+	"ria+${riaurl}#{id}"
 
 datalad save -m "Configure backup siblings and subdataset retrieval"
